83b7c0d418788ce9a9fd3876c663dfa7ba2d1204
Sharan <sharan.techuser@gmail.com>
2023-11-22 22:24:32 +0530

    modified main.go

---
+++ go-palindrom-check/go.mod
@@ -0,0 +1,3 @@
+module github.com/gopalindromcheck
+
+go 1.21.2
--- go-palindrom-check/main.go
+++ go-palindrom-check/main.go
@@ -1,5 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	input := "abcba"
+	res := isPalindrome(input)
+	if res {
+		fmt.Println(input, "is a palindrome")
+	} else {
+		fmt.Println(input, "is not a palindrome")
+	}
+
+}
+
+// return TRUE if a palindrome. else return FALSE
+func isPalindrome(str string) bool {
+	for i := 0; i < len(str); i++ {
+		if i > len(str)-1-i {
+			break
+		}
+		if str[i] != str[len(str)-1-i] {
+			return false
+		}
+	}
+	return true
 }
